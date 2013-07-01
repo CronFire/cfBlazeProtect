4da9f6f3c88f88da66eb3ab4815105634cafb6f6
Zeluboba <zabaluevr@gmail.com>
2013-07-01 13:13:01 +0300

    Final commit.

--- src/ru/cronfire/cfBlazeProtect/cfBlazeProtect.java
+++ src/ru/cronfire/cfBlazeProtect/cfBlazeProtect.java
@@ -27,20 +27,20 @@ public void onEnable() {
 	PluginManager pm = getServer().getPluginManager();
 	pm.registerEvents(this, this);
 
-	log.info("[" + getDescription().getName() + "]" + " Enabled " + getDescription().getName() + " v" + getDescription().getVersion());
+	log.info("[cfBlazeProtect]" + " Enabled cfBlazeProtect v" + getDescription().getVersion());
 }
 
 public void onDisable() {
-	log.info("[" + getDescription().getName() + "]" + " Disabled " + getDescription().getName() + " v" + getDescription().getVersion());
+	log.info("[cfBlazeProtect]" + " Disabled cfBlazeProtect v" + getDescription().getVersion());
 }
 		  
 public void loadConfig() { 
 	this.config = getConfig();
-	config.addDefault("Message", "You are not allowed to do it!");
+	this.config.addDefault("Message", "You are not allowed to do it!");
 
 	config.options().copyDefaults(true);
 	saveConfig();
-	log.info("[" + getDescription().getName() + "]" + " Successfully loaded configuration file.");
+	log.info("[cfBlazeProtect] Successfully loaded configuration file.");
 }
 
 @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
@@ -49,7 +49,7 @@ public void onBlockBreak(BlockBreakEvent event) {
 	String worldname = event.getPlayer().getWorld().getName();
 	Player player = event.getPlayer();
 	
-	if(worldname.equalsIgnoreCase("world_nether") || worldname.contains("nether")) {  //for bukkit users
+	if(worldname.contains("nether")) {
 		if (event.getBlock().getType() == Material.MOB_SPAWNER) {
         	CreatureSpawner spawner = (CreatureSpawner) event.getBlock().getState();
         	
