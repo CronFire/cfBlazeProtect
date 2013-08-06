475abae13fbf305f66433187e046a99b7eb1c58c
Zeluboba <zabaluevr@gmail.com>
2013-08-07 02:57:35 +0300

    Github repo.

--- src/ru/cronfire/cfBlazeProtect/cfBlazeProtect.java
+++ src/ru/cronfire/cfBlazeProtect/cfBlazeProtect.java
@@ -1,9 +1,8 @@
 package ru.cronfire.cfBlazeProtect;
 
-import java.util.logging.Logger;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World.Environment;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
@@ -15,51 +14,49 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
 
 public class cfBlazeProtect extends JavaPlugin implements Listener {
 	public static Logger log = Logger.getLogger("Minecraft");
 	public FileConfiguration config;
 
-public void onEnable() {
-	loadConfig();
-	reloadConfig();
+	public void onEnable() {
+		loadConfig();
+		reloadConfig();
 
-	PluginManager pm = getServer().getPluginManager();
-	pm.registerEvents(this, this);
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
 
-	log.info("[cfBlazeProtect]" + " Enabled cfBlazeProtect v" + getDescription().getVersion());
-}
-
-public void onDisable() {
-	log.info("[cfBlazeProtect]" + " Disabled cfBlazeProtect v" + getDescription().getVersion());
-}
-		  
-public void loadConfig() { 
-	this.config = getConfig();
-	this.config.addDefault("Message", "You are not allowed to do it!");
-
-	config.options().copyDefaults(true);
-	saveConfig();
-	log.info("[cfBlazeProtect] Successfully loaded configuration file.");
-}
-
-@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-public void onBlockBreak(BlockBreakEvent event) {
-	this.config = getConfig();
-	String worldname = event.getPlayer().getWorld().getName();
-	Player player = event.getPlayer();
-	
-	if(worldname.contains("nether")) {
-		if (event.getBlock().getType() == Material.MOB_SPAWNER) {
-        	CreatureSpawner spawner = (CreatureSpawner) event.getBlock().getState();
-        	
-        	if ((spawner.getSpawnedType() == EntityType.BLAZE) && (!player.hasPermission("blazeprotect.exempt"))) {
-            	player.sendMessage(ChatColor.RED + config.getString("Message"));
-            	event.setCancelled(true);
-        	}
-    	}
+		log.info("[cfBlazeProtect]" + " Enabled cfBlazeProtect v" + getDescription().getVersion());
 	}
-    
-}
-		
+
+	public void onDisable() {
+		log.info("[cfBlazeProtect]" + " Disabled cfBlazeProtect v" + getDescription().getVersion());
+	}
+
+	public void loadConfig() {
+		this.config = getConfig();
+		this.config.addDefault("Message", "&cYou are not allowed to do it!");
+
+		config.options().copyDefaults(true);
+		saveConfig();
+		log.info("[cfBlazeProtect] Successfully loaded configuration file.");
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public void blazeProtect(BlockBreakEvent event) {
+		Player pl = event.getPlayer();
+
+		if(pl.getWorld().getEnvironment().equals(Environment.NETHER)) {
+			if(event.getBlock().getType() == Material.MOB_SPAWNER) {
+				CreatureSpawner spawner = (CreatureSpawner) event.getBlock().getState();
+
+				if((spawner.getSpawnedType() == EntityType.BLAZE) && (!pl.hasPermission("cfbundle.blazeprotect.bypass"))) {
+					pl.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.BlazeProtect")));
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+
 }
